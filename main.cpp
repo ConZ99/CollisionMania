990a311be37234538aefefe7895e6265a215fa10
Lica Stefan <stefanlica99@gmail.com>
2022-10-29 19:43:18 +0300

    Window creation

--- main.cpp
+++ main.cpp
@@ -2,8 +2,39 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+static int WINDOW_WIDTH = 800;
+static int WINDOW_HEIGHT = 800;
+
 int main()
 {
-	std::cout << "Hello World!";
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CollisionMania", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	glClearColor(0.00f, 0.30f, 0.10f, 1.00f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
