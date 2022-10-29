0f128337c14328080d6cbc19ef03a6dfc62e9faf
Lica Stefan <stefanlica99@gmail.com>
2022-10-29 20:20:42 +0300

    Draw square

--- main.cpp
+++ main.cpp
@@ -5,14 +5,32 @@
 static int WINDOW_WIDTH = 800;
 static int WINDOW_HEIGHT = 800;
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main()
 {
+	//init GLFW
 	glfwInit();
 
+	//using OpenGL v3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//window creation
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CollisionMania", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create window" << std::endl;
@@ -20,19 +38,73 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-
 	gladLoadGL();
-
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-	glClearColor(0.00f, 0.30f, 0.10f, 1.00f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
+	//create vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//create fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//create shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//shader cleanup
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//square coords
+	GLfloat vertices[] =
+	{
+		-.5f, -.5f, .0f,
+		-.5f,  .5f, .0f,
+		 .5f,  .5f, .0f, //first triangle
+
+		 .5f,  .5f, .0f,
+		 .5f, -.5f, .0f,
+		-.5f, -.5f, .0f //second triangle, total 6 vertices
+	};
+
+	//VAO and VBO setup
+	//ORDER IS VERY IMPORTANT
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//number of values per vertex is 3
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		//window background color, current: forest green
+		glClearColor(0.00f, 0.30f, 0.10f, 1.00f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//choose what shader to use
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	//VAO and VBO cleanup
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
